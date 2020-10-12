5cbe20ead4ce3e3bb4b50bb3c50129368be14c36
Purujeet Singhal <spurujeet@gmail.com>
2020-10-12 22:18:27 +0530

    Update watchtimeinc.py

--- watchtimeinc.py
+++ watchtimeinc.py
@@ -11,25 +11,7 @@ links = [
             'https://www.youtube.com/watch?v=FAkGlmwfLa0',
             'https://www.youtube.com/watch?v=MF0npoiQfFU',
             'https://www.youtube.com/watch?v=2CpuznudFlk',
-            'https://www.youtube.com/watch?v=kuKEvrJFM6o',
-            'https://www.youtube.com/watch?v=qU7y7YdYV-g',
-            'https://www.youtube.com/watch?v=T3ObvO6oAAY',
-            'https://www.youtube.com/watch?v=wf5z_FPdRb8',
-            'https://www.youtube.com/watch?v=uFZtVer4Uiw',
-            'https://www.youtube.com/watch?v=FHS8d7d7pSw',
-            'https://www.youtube.com/watch?v=gGmER2HTBP8',
-            'https://www.youtube.com/watch?v=9EZQJURUp2k',
-            'https://www.youtube.com/watch?v=1M1IeA7277c',
-            'https://www.youtube.com/watch?v=kuKEvrJFM6o',
-            'https://www.youtube.com/watch?v=MhSBeVsL1bw',
-            'https://www.youtube.com/watch?v=r_4uz9YIX9o',
-            'https://www.youtube.com/watch?v=FAkGlmwfLa0',
-            'https://www.youtube.com/watch?v=MF0npoiQfFU',
-            'https://www.youtube.com/watch?v=2CpuznudFlk',
-            'https://www.youtube.com/watch?v=kuKEvrJFM6o',
-            'https://www.youtube.com/watch?v=qU7y7YdYV-g',
-            'https://www.youtube.com/watch?v=Fi405o8kCH0'
-            
+            'https://www.youtube.com/watch?v=kuKEvrJFM6o' 
             ]
 print(len(links))
 
@@ -37,7 +19,7 @@ print(len(links))
 for times in range(hour*6):    
     
 
-    driver = webdriver.Chrome('F://chromedriver.exe')
+    driver = webdriver.Chrome('C://chromedriver.exe')
     driver.get("https://www.youtube.com/watch?v=Zutpvox5ygI")
 
     for link in links:
@@ -48,7 +30,7 @@ for times in range(hour*6):
     for i in range(10):
         for j in range(len(driver.window_handles)):
             if(i==0):
-                driver.switch_to_window(driver.window_handles[j])
+                driver.switch_to_window(driver.window_handles[j-1])
                 try :
                     element2 = driver.find_element_by_link_text( "No thanks").click()    
                     print('No thanks is clicked')
@@ -62,7 +44,6 @@ for times in range(hour*6):
                     print(e)
                     
                 try:
-                    driver.find_element_by_xpath("//button[@aria-label='Settings'").click()
 
                     element = driver.find_element_by_xpath("//button[@aria-label='Play']")
                     element.click()
@@ -71,8 +52,8 @@ for times in range(hour*6):
                 #body = driver.find_element_by_tag_name("body")
                 #body.send_keys(Keys.SPACE)
                 print('Tab switched:',j)
-                #time.sleep(5)
-        #time.sleep(60)
+                time.sleep(5)
+        time.sleep(60)
         print(i+1,' minute over')
     print('Refreshing Tabs')
     driver.quit()
